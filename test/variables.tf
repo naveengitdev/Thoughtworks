efef64d65dd6302203a3a602c070876ae130a0b8
naveengitdev <140637709+naveengitdev@users.noreply.github.com>
2024-04-06 11:44:39 +0530

    Update variables.tf

--- test/variables.tf
+++ test/variables.tf
@@ -1,1 +1,37 @@
+variable "rg" {
+  description = "RG Name"
+  type = string
+  default = ""
+}
 
+variable "rg_location" {
+  description = "RG Location"
+  type = string
+  default = ""
+}
+
+variable "vnet_name" {
+    description = "Vnet name"
+    type = string
+    default = ""
+}
+
+variable "subnet_name" {
+    description = "Subnet name"
+    type = string
+    default = ""
+}
+
+
+variable "nic_name" {
+    description = "NIC name"
+    type = string
+    default = ""
+}
+
+
+variable "vm_name" {
+    description = "VM name"
+    type = string
+    default = ""
+}
