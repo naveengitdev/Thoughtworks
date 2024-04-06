6c7cece4c44aa39f0b4545d6d02cce2da3c6d752
naveengitdev <140637709+naveengitdev@users.noreply.github.com>
2024-04-06 11:45:57 +0530

    Update vm.tf

--- module/vm.tf
+++ module/vm.tf
@@ -1,1 +1,9 @@
-
+module "test-vm" {
+  source = "../test"
+  rg = "test-rg"
+  rg_location = "centralindia"
+  subnet_name = "default-subnet"
+  vnet_name = "test-vnet"
+  nic_name = "test-nic"
+  vm_name = "test-vm"
+}
