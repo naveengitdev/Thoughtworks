84dcaa88f3bc21c08b17d13bec3b4f3e5baae845
naveengitdev <140637709+naveengitdev@users.noreply.github.com>
2024-04-06 11:43:39 +0530

    Delete vm.tf

--- vm.tf
+++
@@ -1,9 +0,0 @@
-module "test-vm" {
-  source = "../test"
-  rg = "test-rg"
-  rg_location = "centralindia"
-  subnet_name = "default-subnet"
-  vnet_name = "test-vnet"
-  nic_name = "test-nic"
-  vm_name = "test-vm"
-}
